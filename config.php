c14e1ad2812b72374994e5773311aaa8eaeaa0f0
Hoang Yen <yenvhgcs18147@fpt.edu.vn>
2020-11-02 18:34:23 +0700

    6th upload

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
-<?php
+<?php 
+    function getDB() {
+        return $db = pg_connect("host=ec2-34-200-72-77.compute-1.amazonaws.com
+                          dbname=d58oke6ofk8nhk
+                          port=5432
+                          user=gwvgokvrzcsmjo
+                          password=762220269f462ac464b587649743b3b1b95a91b112f570e565702a8bfe8181ed")
+                          or die("Connection failed!");
+    }
 
-define('DB_SERVER', 'ec2-34-200-72-77.compute-1.amazonaws.com');
-define('DB_USERNAME', 'gwvgokvrzcsmjo');
-define('DB_PASSWORD', '762220269f462ac464b587649743b3b1b95a91b112f570e565702a8bfe8181ed');
-define('DB_NAME', 'd58oke6ofk8nhk');
- 
-/* Attempt to connect to PostgreSQL database */
-$link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
-		or die('Could not connect1: ' . pg_last_error());
 
-header('Location:indexhtm;.php');
-?>
+    header('Location:indexhtml.php');
+ ?>
