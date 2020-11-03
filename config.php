4eb92b4bce0a8499dadb9b374826cbdd461c9d85
yenvhgcs18147 <66456726+yenvhgcs18147@users.noreply.github.com>
2020-11-03 11:21:13 +0700

    Update config.php

--- config.php
+++ config.php
@@ -8,14 +8,5 @@
                           or die("Connection failed!");
     }
 
-    switch($db) {
-        case '/':
-            require __DIR__ . 'index.php';
-            break;
-        default:
-            http_response_code(404);
-            require __DIR__ . '/views/404.php';
-            break;
-    }
     header('Location:indexhtml.php');
  ?>
